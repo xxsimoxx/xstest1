d43008718aee6b786bf7ffdff3e927c12c674fbd
Simone Fioravanti <simospa@gieffeedizioni.it>
2025-08-26 15:59:38 +0200

    Update xstest1.php

--- xstest1.php
+++ xstest1.php
@@ -6,7 +6,7 @@
 	Author URI:   https://simonefioravanti.it
 	Text Domain:  limit-login-attempts
 	Domain Path:  /languages
-	Version:      1.0.0
+	Version:      1.1.0
 	Requires CP:  2.0
 	Requires PHP: 7.4
 */
