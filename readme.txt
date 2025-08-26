eac431e7ad9e47a271a8909524a5ea9d838fa6bd
xxsimoxx <simone@gieffeedizioni.it>
2025-08-26 10:03:47 +0200

    First

---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Nothing here 1 ===
+Tags: login, security, authentication
+Requires at least: 2.8
+Requires CP: 2.0
+Requires PHP: 7.4
+
+Limit rate of login attempts, including by way of cookies, for each IP. Fully customizable.
+
+== Description ==
+
+ciao da readme.txt!
---
+++ xstest1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	Plugin Name:  XS Test 1
+	Description:  Test plugin
+	Author:       Simone Fioravanti
+	Author URI:   https://simonefioravanti.it
+	Text Domain:  limit-login-attempts
+	Domain Path:  /languages
+	Version:      1.0.0
+	Requires CP:  2.0
+	Requires PHP: 7.4
+*/
+
+namespace xxsimoxx\nothing;
+
+define('THISPLUGINDOESNOTHING', true);
